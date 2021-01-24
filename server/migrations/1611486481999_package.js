7e8b7b4fd45e14a004521c67a255ccf85604368a
vattcarter7 <vattcarter@gmail.com>
2021-01-24 18:11:59 +0700

    added package table migration

---
+++ server/migrations/1611486481999_package.js
@@ -0,0 +1,21 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.sql(`
+    CREATE TABLE packages (
+      id SERIAL PRIMARY KEY,
+      name VARCHAR,
+      quantity JSON,
+      created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+      updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    );
+  `);
+};
+
+exports.down = (pgm) => {
+  pgm.sql(`
+    DROP TABLE packages;
+  `);
+};
