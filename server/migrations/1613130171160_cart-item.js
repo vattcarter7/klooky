352b4e72af6e049bc68c92cf91b8597a0f5a2abf
vattcarter7 <vattcarter@gmail.com>
2021-02-12 18:46:38 +0700

    added cart_items table migration

---
+++ server/migrations/1613130171160_cart-item.js
@@ -0,0 +1,21 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  //TODO: add product_id, user_id on the cart_items table
+  pgm.sql(`
+    CREATE TABLE cart_items (
+      id SERIAL PRIMARY KEY,
+      quantity_price_modle JSONB,
+      created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+      updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    );
+  `);
+};
+
+exports.down = (pgm) => {
+  pgm.sql(`
+    DROP TABLE cart_items;
+  `);
+};
