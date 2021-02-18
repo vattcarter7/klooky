08ecd28ea376f5c2ed83b5755865869343d71c48
vattcarter7 <vattcarter@gmail.com>
2021-02-18 15:24:12 +0700

    editPackageDetailCheckRules on package validator

--- server/src/validators/package/packageCheckRules.js
+++ server/src/validators/package/packageCheckRules.js
@@ -18,6 +18,28 @@ exports.createPackageDetailCheckRules = (() => {
   return [
     check('includes').not().isEmpty().withMessage('inclusive is required'),
     check('excludes').not().isEmpty().withMessage('exclusive is required'),
-    check('itinerary').not().isEmpty().withMessage('itenerary is required')
+    check('itinerary').not().isEmpty().withMessage('itenerary is required'),
+    check('package_duration')
+      .isNumeric()
+      .withMessage('package duration must be number and is required'),
+    check('free_cancelation_max_day')
+      .optional()
+      .isInt()
+      .withMessage('free cancelation max day must be integer number')
+  ];
+})();
+
+exports.editPackageDetailCheckRules = (() => {
+  //TODO add packageId and check for the empty packageId
+  return [
+    check('includes').not().isEmpty().withMessage('inclusive is required'),
+    check('package_duration')
+      .optional()
+      .isNumeric()
+      .withMessage('package duration must be number'),
+    check('free_cancelation_max_day')
+      .optional()
+      .isInt()
+      .withMessage('free cancelation max day must be integer number')
   ];
 })();
