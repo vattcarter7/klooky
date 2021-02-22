13a5bd8a4e593bf9c525e72e01da2b5379cf376d
vattcarter7 <vattcarter@gmail.com>
2021-02-22 19:13:04 +0700

    added photos column on product-reviews table

--- server/migrations/1613995491853_product-review.js
+++ server/migrations/1613995491853_product-review.js
@@ -3,12 +3,13 @@
 exports.shorthands = undefined;
 
 exports.up = (pgm) => {
-  //TODO: add product_id, user_id on the purchases table
+  //TODO: add product_id, reviewer_id on the purchases table
   pgm.sql(`
     CREATE TABLE product_reviews (
       id SERIAL PRIMARY KEY,
       rating SMALLINT NOT NULL,
       comment VARCHAR,
+      photos JSONB,
       created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
       updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
     );
