a5f69c4df9f317c2e938d94bdd9cd91417292f79
vattcarter7 <vattcarter@gmail.com>
2021-02-27 20:31:06 +0700

    added product controller

---
+++ server/src/controllers/product.js
@@ -0,0 +1,20 @@
+const pool = require('../pool');
+const { toPgTimestamp } = require('../utils/time-util');
+
+exports.createProduct = async (req, res) => {
+  try {
+    const {
+      rows
+    } = await pool.query(
+      'INSERT INTO products (name) VALUES ($1) RETURNING *;',
+      [req.body.name]
+    );
+
+    res.send(rows[0]);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({
+      err: err.message
+    });
+  }
+};
