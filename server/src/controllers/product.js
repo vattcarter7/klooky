80da970f4b7af594a1a12d6627179a7162a88409
vattcarter7 <vattcarter@gmail.com>
2021-03-01 18:08:51 +0700

    editProduct controller

--- server/src/controllers/product.js
+++ server/src/controllers/product.js
@@ -19,4 +19,41 @@ exports.createProduct = async (req, res) => {
   }
 };
 
-//TODO: edit a product
+exports.editProduct = async (req, res) => {
+  try {
+    const textQuery = `SELECT * FROM products where id=$1`;
+    const response = await pool.query(textQuery, [req.params.id]);
+    if (!response.rows[0]) {
+      return res.status(404).json({
+        errorMsg: 'product not found'
+      });
+    }
+
+    const updateQuery = `UPDATE packages_details SET
+                          name                            =$1,
+                          updated_at                      =to_timestamp($2)
+                        WHERE id = $3 returning *;
+                        `;
+
+    const updateValues = [
+      req.body.name,
+      toPgTimestamp(Date.now()),
+      req.params.id
+    ];
+
+    const { rows } = await pool.query(updateQuery, updateValues);
+    if (!rows[0]) {
+      return res.status(400).json({
+        err: err.message,
+        errorMsg: 'unable to update package detail'
+      });
+    }
+
+    res.send(rows[0]);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({
+      errorMsg: err.message
+    });
+  }
+};
