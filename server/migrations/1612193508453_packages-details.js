fd3ef9844d1f1b1e7177b311f61cd9934b4a08e4
vattcarter7 <vattcarter@gmail.com>
2021-03-13 00:45:08 +0700

    modified package_details table

--- server/migrations/1612193508453_packages-details.js
+++ server/migrations/1612193508453_packages-details.js
@@ -9,9 +9,9 @@ exports.up = (pgm) => {
       package_includes JSONB,
       package_excludes JSONB,
       package_itinerary JSONB,
-      package_additional_info JSONB,
       package_duration INT, 
       package_pickup_info JSONB,
+      package_additional_info JSONB,
       created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
       updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
     );
