af37857c75521d0697836e617dbf2ac5ebe416f4
vattcarter7 <vattcarter@gmail.com>
2021-03-25 22:52:07 +0700

    added package-detail-locale table

---
+++ server/migrations/1612193508453_package-detail-locale.js
@@ -0,0 +1,29 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.sql(`
+    CREATE TABLE package_detail_locale (
+      id SERIAL PRIMARY KEY,
+      package_id PK FK,
+      language_id PK FK,
+      package_name VARCHAR,
+      package_includes JSONB,
+      package_excludes JSONB,
+      package_itinerary JSONB,
+      package_pickup_info JSONB,
+      package_additional_info JSON,
+      created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+      updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    );
+
+    ALTER TABLE package_detail_locale ADD CONSTRAINT package_detail_locale_unique UNIQUE (package_id, language_id);
+  `);
+};
+
+exports.down = (pgm) => {
+  pgm.sql(`
+    DROP TABLE package_detail_locale;
+  `);
+};
