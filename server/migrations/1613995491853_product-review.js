308e586ed918e6a4c87d732cc17419e1541073a1
vattcarter7 <vattcarter@gmail.com>
2021-03-29 00:59:03 +0700

    added reviewer_id and host_id on product_review table

--- server/migrations/1613995491853_product-review.js
+++ server/migrations/1613995491853_product-review.js
@@ -3,11 +3,12 @@
 exports.shorthands = undefined;
 
 exports.up = (pgm) => {
-  //TODO: add reviewer_id, host_id on the product_reviews table
   pgm.sql(`
     CREATE TABLE product_review (
       id SERIAL PRIMARY KEY,
       product_id INT REFERENCES product(id) NOT NULL,
+      reviewer_id INT REFERENCES users(id) NOT NULL,
+      host_id INT REFERENCES users(id) NOT NULL,
       rating SMALLINT NOT NULL,
       comment VARCHAR,
       created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
