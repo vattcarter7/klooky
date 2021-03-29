29ea2026418da881beee80d67c50317de7240b8b
vattcarter7 <vattcarter@gmail.com>
2021-03-29 20:45:46 +0700

    seed data for product_review_photo table

--- server/migrations/1616689421067_product-review-photo.js
+++ server/migrations/1616689421067_product-review-photo.js
@@ -3,15 +3,19 @@
 exports.shorthands = undefined;
 
 exports.up = (pgm) => {
-  //TODO: add reviewer_id on the product_reviews table
   pgm.sql(`
     CREATE TABLE product_review_photo (
       id SERIAL PRIMARY KEY,
       product_id INT REFERENCES product(id) NOT NULL,
+      reviewer_id INT REFERENCES users(id) NOT NULL,
       photo_url TEXT NOT NULL,
       created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
       updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
     );
+
+    INSERT INTO product_review_photo (product_id, reviewer_id, photo_url)
+    VALUES (1, 1, 'image1.jpg'),
+           (1, 1, 'image2.jpg');
   `);
 };
 
