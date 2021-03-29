6e0697b22c41f6165c78bb69c6056b01e2fd51cd
vattcarter7 <vattcarter@gmail.com>
2021-03-29 20:29:25 +0700

    add user_id to cart_item table

--- server/migrations/1616688005114_cart-item.js
+++ server/migrations/1616688005114_cart-item.js
@@ -3,11 +3,11 @@
 exports.shorthands = undefined;
 
 exports.up = (pgm) => {
-  // TODO: add user_id INT REFERENCES user(id) NOT NULL,
   pgm.sql(`
     CREATE TABLE cart_item (
       id SERIAL PRIMARY KEY,
       package_id INT REFERENCES package(id) NOT NULL,
+      user_id INT REFERENCES users(id) NOT NULL,
       quantity_price_model JSONB NOT NULL,
       created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
       updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
