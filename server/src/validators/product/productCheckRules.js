8fa9082d80705bb74a7cfac1a0c2679948860188
vattcarter7 <vattcarter@gmail.com>
2021-04-02 16:04:20 +0700

    modified productCheckRules

--- server/src/validators/product/productCheckRules.js
+++ server/src/validators/product/productCheckRules.js
@@ -1,5 +1,18 @@
 const { check } = require('express-validator');
 
 exports.createProductCheckRules = (() => {
-  return [check('name').not().isEmpty().withMessage('name is required')];
+  return [
+    check('product_duration')
+      .not()
+      .isEmpty()
+      .withMessage('product duration is required'),
+    check('product_location.*.lat')
+      .optional()
+      .isInt()
+      .withMessage('latitude must be integer number'),
+    check('product_location.*.lng')
+      .optional()
+      .isInt()
+      .withMessage('longitude must be integer number')
+  ];
 })();
