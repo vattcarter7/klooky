5f1a86236442ec5ceb6f463d0188c4e8453a8159
vattcarter7 <vattcarter@gmail.com>
2021-04-04 14:07:02 +0700

    boolean check rules on add product

--- server/src/validators/product/productCheckRules.js
+++ server/src/validators/product/productCheckRules.js
@@ -6,11 +6,48 @@ exports.createProductCheckRules = (() => {
       .not()
       .isEmpty()
       .withMessage('product duration is required'),
+
     check('product_location.*.lat')
       .isInt()
       .withMessage('latitude must be integer number'),
+
     check('product_location.*.lng')
       .isInt()
-      .withMessage('longitude must be integer number')
+      .withMessage('longitude must be integer number'),
+
+    check('product_free_cancelation_max_day')
+      .optional()
+      .isInt()
+      .withMessage('product cancellation max day must be integer number'),
+
+    check('published')
+      .optional()
+      .isBoolean()
+      .withMessage('published must be boolean'),
+
+    check('is_pickup')
+      .optional()
+      .isBoolean()
+      .withMessage('is_pickup must be boolean'),
+
+    check('is_fixed_date_ticket')
+      .optional()
+      .isBoolean()
+      .withMessage('is_fixed_date_ticket must be boolean'),
+
+    check('is_location_meetup')
+      .optional()
+      .isBoolean()
+      .withMessage('is_location_meetup must be boolean'),
+
+    check('is_joined_and_private_available')
+      .optional()
+      .isBoolean()
+      .withMessage('is_joined_and_private_available must be boolean'),
+
+    check('is_hotel_pickup')
+      .optional()
+      .isBoolean()
+      .withMessage('is_hotel_pickup must be boolean')
   ];
 })();
