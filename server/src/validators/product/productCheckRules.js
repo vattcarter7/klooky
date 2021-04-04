cf107a93bbfabfafb8059fa88fd66414b6d8b66e
vattcarter7 <vattcarter@gmail.com>
2021-04-04 14:16:15 +0700

    edit product check rules

--- server/src/validators/product/productCheckRules.js
+++ server/src/validators/product/productCheckRules.js
@@ -1,5 +1,6 @@
 const { check } = require('express-validator');
 
+//-- check rules for creating a product
 exports.createProductCheckRules = (() => {
   return [
     check('product_duration')
@@ -11,6 +12,61 @@ exports.createProductCheckRules = (() => {
       .withMessage('latitude must be integer number'),
 
     check('product_location.*.lng')
+      .isInt()
+      .withMessage('longitude must be integer number'),
+
+    check('product_free_cancelation_max_day')
+      .optional()
+      .isInt()
+      .withMessage('product cancellation max day must be integer number'),
+
+    check('published')
+      .optional()
+      .isBoolean()
+      .withMessage('published must be boolean'),
+
+    check('is_pickup')
+      .optional()
+      .isBoolean()
+      .withMessage('is_pickup must be boolean'),
+
+    check('is_fixed_date_ticket')
+      .optional()
+      .isBoolean()
+      .withMessage('is_fixed_date_ticket must be boolean'),
+
+    check('is_location_meetup')
+      .optional()
+      .isBoolean()
+      .withMessage('is_location_meetup must be boolean'),
+
+    check('is_joined_and_private_available')
+      .optional()
+      .isBoolean()
+      .withMessage('is_joined_and_private_available must be boolean'),
+
+    check('is_hotel_pickup')
+      .optional()
+      .isBoolean()
+      .withMessage('is_hotel_pickup must be boolean')
+  ];
+})();
+
+//-- check rules for editting a product
+exports.editProductCheckRules = (() => {
+  return [
+    check('product_duration')
+      .optional()
+      .isInt()
+      .withMessage('product duration is integer and required'),
+
+    check('product_location.*.lat')
+      .optional()
+      .isInt()
+      .withMessage('latitude must be integer number'),
+
+    check('product_location.*.lng')
+      .optional()
       .isInt()
       .withMessage('longitude must be integer number'),
 
