2087f97d0350447fa3f52a110f8de52c5ff92be5
vattcarter7 <vattcarter@gmail.com>
2021-04-08 14:26:20 +0700

    modified produc locale controller

--- server/src/controllers/product-locale.js
+++ server/src/controllers/product-locale.js
@@ -9,24 +9,20 @@ exports.createProductLocale = async (req, res) => {
         product_id,
         language_id,
         product_name,
-        product_validity,
+        product_validity_period,
         product_overview,
         product_highlights,
         product_prohibition_limitation,
-        product_confirmation,
-        product_cancelation_policy, 
         product_additional_info
-      ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *;`,
+      ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *;`,
       [
         req.body.product_id,
         req.body.language_id,
         req.body.product_name,
-        JSON.stringify(req.body.product_validity),
+        req.body.product_validity_period,
         JSON.stringify(req.body.product_overview),
         JSON.stringify(req.body.product_highlights),
         JSON.stringify(req.body.product_prohibition_limitation),
-        JSON.stringify(req.body.product_confirmation),
-        JSON.stringify(req.body.product_cancelation_policy),
         JSON.stringify(req.body.product_additional_info)
       ]
     );
