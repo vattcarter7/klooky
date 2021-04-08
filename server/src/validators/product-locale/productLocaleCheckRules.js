36f568d0c44e7425a29b953af4e338bf679c2136
vattcarter7 <vattcarter@gmail.com>
2021-04-09 00:10:26 +0700

    productLocaleCheckRules

---
+++ server/src/validators/product-locale/productLocaleCheckRules.js
@@ -0,0 +1,23 @@
+const { check } = require('express-validator');
+
+//-- check rules for creating a product locale
+exports.createProductLocaleCheckRules = (() => {
+  return [
+    check('product_id').isInt().withMessage('a valid product ID is required'),
+
+    check('language_id').isInt().withMessage('a valid language ID is required'),
+
+    check('product_name')
+      .isString()
+      .withMessage('a valid product name is required'),
+
+    check('product_overview')
+      .isString()
+      .withMessage('a valid product overview is required'),
+
+    check('product_highlights')
+      .not()
+      .isEmpty()
+      .withMessage('product highlights required')
+  ];
+})();
