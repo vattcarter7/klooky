3342e911da2ebc06151e1c600f1a454d76b908e7
vattcarter7 <vattcarter@gmail.com>
2021-04-10 18:06:14 +0700

    added product_id on package table

--- server/migrations/1611486481999_package.js
+++ server/migrations/1611486481999_package.js
@@ -6,12 +6,14 @@ exports.up = (pgm) => {
   pgm.sql(`
     CREATE TABLE package (
       id SERIAL PRIMARY KEY,
+      product_id INT REFERENCES product(id) NOT NULL,
       published BOOLEAN DEFAULT FALSE,
       created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
       updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
     );
 
-    INSERT INTO package (published) VALUES (true);
+    INSERT INTO package (product_id, published) VALUES (1, true),
+                                                       (1, true);
   `);
 };
 
