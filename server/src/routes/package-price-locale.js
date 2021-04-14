81c95a3eba1de675cc7023e3adfb2a3076674161
vattcarter7 <vattcarter@gmail.com>
2021-04-15 00:07:32 +0700

    validate route on edit package price locale

--- server/src/routes/package-price-locale.js
+++ server/src/routes/package-price-locale.js
@@ -3,14 +3,16 @@ const router = express.Router();
 
 const { validate } = require('../validators');
 const {
-  createPackagePriceLocaleCheckRules
+  createPackagePriceLocaleCheckRules,
+  editPackagePriceLocaleCheckRules
 } = require('../validators/package-price-locale/package-price-locale-check-rules');
 
 // middlewares
 
 // controller
 const {
-  createPackagePriceLocale
+  createPackagePriceLocale,
+  editPackagePriceLocale
 } = require('../controllers/package-price-locale');
 
 // @desc      make a package price locale
@@ -22,13 +24,13 @@ router.post(
   createPackagePriceLocale
 );
 
-// @desc      edit a package detail locale
-// @route     PUT /api/package-detail-locale/:id
+// @desc      edit a package price locale
+// @route     PUT /api/package-price-locale/:id
 // @access    Private
-// router.put(
-//   '/package-detail-locale/:id',
-//   [editPackageDetailLocaleCheckRules, validate],
-//   editPackageDetailLocale
-// );
+router.put(
+  '/package-price-locale/:id',
+  [editPackagePriceLocaleCheckRules, validate],
+  editPackagePriceLocale
+);
 
 module.exports = router;
