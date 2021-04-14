7242b2f71af1d6f806aca4ac140d412a30e4e7d8
vattcarter7 <vattcarter@gmail.com>
2021-04-15 00:05:56 +0700

    implemented edit package price locale

--- server/src/controllers/package-price-locale.js
+++ server/src/controllers/package-price-locale.js
@@ -9,12 +9,14 @@ exports.createPackagePriceLocale = async (req, res) => {
       (
         package_id,
         language_id,
-        price_model
-      ) VALUES ($1, $2, $3) RETURNING *;`,
+        package_price_name,
+        price
+      ) VALUES ($1, $2, $3, $4) RETURNING *;`,
       [
         req.body.package_id,
         req.body.language_id,
-        JSON.stringify(req.body.price_model)
+        req.body.package_price_name,
+        req.body.price
       ]
     );
 
@@ -23,11 +25,64 @@ exports.createPackagePriceLocale = async (req, res) => {
     console.log(err);
     res.status(400).json({
       err: err.message,
-      errorMsg:
-        err.message ===
-        'duplicate key value violates unique constraint "package_price_locale_unique"'
-          ? 'this package price with the given language already existed'
-          : 'unable to add a package price locale'
+      errorMsg: 'unable to add a package price locale'
+    });
+  }
+};
+
+// edit a package price locale
+exports.editPackagePriceLocale = async (req, res) => {
+  try {
+    const textQuery = `SELECT * FROM package_price_locale where id=$1`;
+    const response = await pool.query(textQuery, [req.params.id]);
+    if (!response.rows[0]) {
+      return res.status(404).json({
+        errorMsg: 'package price locale not found'
+      });
+    }
+
+    const updateQuery = `UPDATE package_price_locale SET
+                            package_id                    = $1,
+                            language_id                   = $2,
+                            package_price_name            = $3,
+                            price                         = $4,
+                            updated_at                    = to_timestamp($5)
+                         WHERE id = $6 returning *;
+                        `;
+
+    const updateValues = [
+      req.body.package_id === undefined
+        ? response.rows[0].package_id
+        : req.body.package_id,
+
+      req.body.language_id === undefined
+        ? response.rows[0].language_id
+        : req.body.language_id,
+
+      req.body.package_price_name === undefined
+        ? response.rows[0].package_price_name
+        : req.body.package_price_name,
+
+      req.body.price === undefined ? response.rows[0].price : req.body.price,
+
+      toPgTimestamp(Date.now()),
+
+      req.params.id
+    ];
+
+    const { rows } = await pool.query(updateQuery, updateValues);
+    if (!rows[0]) {
+      return res.status(400).json({
+        errorMsg: 'unable to update package price locale'
+      });
+    }
+
+    res.send(rows[0]);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({
+      err: err.message,
+      errorMsg: 'unable to update package price locale'
     });
   }
 };
