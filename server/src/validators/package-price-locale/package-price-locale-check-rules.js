15d23a545b5605babfd93d4cdd06c535e7b3734f
vattcarter7 <vattcarter@gmail.com>
2021-04-15 00:07:08 +0700

    implemented edit package price locale check rules

--- server/src/validators/package-price-locale/package-price-locale-check-rules.js
+++ server/src/validators/package-price-locale/package-price-locale-check-rules.js
@@ -4,10 +4,21 @@ exports.createPackagePriceLocaleCheckRules = (() => {
   return [
     check('package_id').not().isEmpty().withMessage('package ID is required'),
     check('language_id').not().isEmpty().withMessage('language ID is required'),
-    check('price_model.name')
+    check('package_price_name')
       .isString()
       .withMessage('a valid package price name is required'),
-    check('price_model.price')
+    check('price').isNumeric().withMessage('a valid package price is required')
+  ];
+})();
+
+exports.editPackagePriceLocaleCheckRules = (() => {
+  return [
+    check('package_price_name')
+      .optional()
+      .isString()
+      .withMessage('a valid package price name is required'),
+    check('price')
+      .optional()
       .isNumeric()
       .withMessage('a valid package price is required')
   ];
