2acb3b4568ea18759df162fa522a0b693f5a8322
vattcarter7 <vattcarter@gmail.com>
2021-04-16 23:00:08 +0700

    modified add cart item controller

--- server/src/controllers/cart.js
+++ server/src/controllers/cart.js
@@ -3,12 +3,18 @@ const { toPgTimestamp } = require('../utils/time-util');
 
 exports.createCartItem = async (req, res) => {
   try {
-    const {
-      rows
-    } = await pool.query(
-      'INSERT INTO cart_item (package_id, user_id, quantity_price_model) VALUES ($1, $2, $3) RETURNING *;',
+    const { rows } = await pool.query(
+      `INSERT INTO cart_item 
+      (
+        product_locale_id,
+        package_detail_locale_id,
+        user_id, 
+        quantity_price_model
+      ) 
+       VALUES ($1, $2, $3, $4) RETURNING *;`,
       [
-        req.body.package_id,
+        req.body.product_locale_id,
+        req.body.package_detail_locale_id,
         req.body.user_id,
         JSON.stringify(req.body.quantity_price_model)
       ]
