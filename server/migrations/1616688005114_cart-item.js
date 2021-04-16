996002f6dc31b08f86fb08272316ba06519ca448
vattcarter7 <vattcarter@gmail.com>
2021-04-16 22:59:33 +0700

    modified cart item table

--- server/migrations/1616688005114_cart-item.js
+++ server/migrations/1616688005114_cart-item.js
@@ -6,19 +6,21 @@ exports.up = (pgm) => {
   pgm.sql(`
     CREATE TABLE cart_item (
       id SERIAL PRIMARY KEY,
-      package_id INT REFERENCES package(id) NOT NULL,
+      product_locale_id INT REFERENCES product_locale(id) NOT NULL,
+      package_detail_locale_id INT REFERENCES package_detail_locale(id) NOT NULL,
       user_id INT REFERENCES users(id) NOT NULL,
       quantity_price_model JSONB NOT NULL,
       created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
       updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
     );
 
-    CREATE INDEX cart_item_package_id_idx ON cart_item(package_id);
+    CREATE INDEX cart_item_product_locale_id_idx ON cart_item(product_locale_id);
+    CREATE INDEX cart_item_package_detail_locale_id_idx ON cart_item(package_detail_locale_id);
     CREATE INDEX cart_item_user_id_idx ON cart_item(user_id);
 
-    INSERT INTO cart_item (package_id, user_id, quantity_price_model)
-    VALUES (1, 1, '[{ "name": "adult", "price": 15, "pax": 3 }, { "name": "kid", "price": 10, "pax": 1 }]'),
-           (1, 1, '[{ "name": "성인", "price": 15, "pax": 3 }, { "name": "아동", "price": 10, "pax": 1 }]');
+    INSERT INTO cart_item (product_locale_id, package_detail_locale_id, user_id, quantity_price_model)
+    VALUES (1, 1, 1, '[{ "name": "adult", "price": 15, "pax": 3 }, { "name": "kid", "price": 10, "pax": 1 }]'),
+           (1, 2, 2, '[{ "name": "성인", "price": 15, "pax": 3 }, { "name": "아동", "price": 10, "pax": 1 }]');
   `);
 };
 
