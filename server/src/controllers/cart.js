b66767bc28e92c07e4e3bee53c9617da6a598dd8
vattcarter7 <vattcarter@gmail.com>
2021-04-16 21:37:50 +0700

    create cart item controller

--- server/src/controllers/cart.js
+++ server/src/controllers/cart.js
@@ -6,15 +6,20 @@ exports.createCartItem = async (req, res) => {
     const {
       rows
     } = await pool.query(
-      'INSERT INTO cart_items (quantity_price_model) VALUES ($1) RETURNING *;',
-      [JSON.stringify(req.body.quantity_price_model)]
+      'INSERT INTO cart_item (package_id, user_id, quantity_price_model) VALUES ($1, $2, $3) RETURNING *;',
+      [
+        req.body.package_id,
+        req.body.user_id,
+        JSON.stringify(req.body.quantity_price_model)
+      ]
     );
 
     res.send(rows[0]);
   } catch (err) {
     console.log(err);
     res.status(400).json({
-      err: err.message
+      err: err.message,
+      errorMsg: 'unable to add a cart item'
     });
   }
 };
