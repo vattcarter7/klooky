e1d5d6960d83ce138cb20bfc82db881f29900677
vattcarter7 <vattcarter@gmail.com>
2021-04-17 22:48:09 +0700

    purchase controller

--- server/src/controllers/purchase.js
+++ server/src/controllers/purchase.js
@@ -3,11 +3,20 @@ const { toPgTimestamp } = require('../utils/time-util');
 
 exports.createPurchase = async (req, res) => {
   try {
-    const {
-      rows
-    } = await pool.query(
-      'INSERT INTO purchases (quantity_price_model, discount, total) VALUES ($1, $2, $3) RETURNING *;',
+    const { rows } = await pool.query(
+      `INSERT INTO purchases 
+      (
+        product_locale_id, 
+        package_detail_locale_id, 
+        quantity_price_model, 
+        user_id,
+        discount, 
+        total
+      ) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;`,
       [
+        req.body.product_locale_id,
+        req.body.package_detail_locale_id,
+        req.body.user_id,
         JSON.stringify(req.body.quantity_price_model),
         req.body.discount,
         req.body.total
