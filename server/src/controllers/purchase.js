3bba54b001c62d2ba12ec2d121327845ee0e53ad
vattcarter7 <vattcarter@gmail.com>
2021-04-21 19:58:14 +0700

    get cartItemResponse on purchase controller

--- server/src/controllers/purchase.js
+++ server/src/controllers/purchase.js
@@ -4,6 +4,25 @@ const { toPgTimestamp } = require('../utils/time-util');
 // add purchase order
 exports.createPurchase = async (req, res) => {
   try {
+    // make sure the cart item quantity is greater than 0
+    const cartItemQuery = `SELECT
+                              product_locale_id,
+                              package_detail_locale_id,
+                              user_id,
+                              quantity_price_model
+                          FROM cart_item
+                          WHERE user_id = $1;
+                          `;
+    // TODO: change value or cartItemValue from 1 to req.user.id with middleware
+    const cartItemValue = [1];
+
+    const cartItemResponse = await pool.query(cartItemQuery, cartItemValue);
+
+    console.log(cartItemResponse.rows);
+
+    return res.send(cartItemResponse.rows);
+
+    // add to purchase table
     const { rows } = await pool.query(
       `INSERT INTO purchases 
       (
