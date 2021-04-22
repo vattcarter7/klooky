ac14389ee3f05fda85f659c11a0e94e9a9b5a85f
vattcarter7 <vattcarter@gmail.com>
2021-04-22 15:43:55 +0700

    aadd purchase table

--- server/src/controllers/purchase.js
+++ server/src/controllers/purchase.js
@@ -1,9 +1,10 @@
+const format = require('pg-format');
 const pool = require('../pool');
 const { toPgTimestamp } = require('../utils/time-util');
 
 // add purchase order
 exports.createPurchase = async (req, res) => {
-  // TODO: make a transaction for database
+  await pool.query('BEGIN');
   try {
     // make sure the cart item quantity is greater than 0
     const cartItemQuery = `SELECT
@@ -25,6 +26,8 @@ exports.createPurchase = async (req, res) => {
       });
     }
 
+    // *** the purchase values must be strictly formed by fields in order
+    // *** product_locale_id, package_detail_locale_id, user_id, quantity_price_model, discount, total
     let purchaseValues = [];
 
     cartItemResponse.rows.map((v) => {
@@ -32,45 +35,39 @@ exports.createPurchase = async (req, res) => {
       v.quantity_price_model.map((p) => {
         cartItemTotalPrice += p.pax * p.price;
       });
-      // TODO: add v.discount here
+      // TODO: modified v.discount here
+      v.quantity_price_model = { ...v.quantity_price_model };
+      v.discount = 0;
       v.total = cartItemTotalPrice;
       purchaseValues.push(Object.values(v));
     });
 
     console.log('PURCHASE_VALUES:', JSON.stringify(purchaseValues, null, 2));
 
-    // cartItemResponse.rows[3].quantity_price_model.map((p) => {
-    //   cartItemTotalPrice += p.pax * p.price;
-    // });
-
-    // console.log('cartItemTotalPrice', cartItemTotalPrice);
-
-    return res.send(cartItemResponse.rows);
-
     // add to purchase table
-    const { rows } = await pool.query(
-      `INSERT INTO purchases 
+    // 1. insert into table purchase with cartItemResponse
+    const purchaseQuery = format(
+      `INSERT INTO purchase 
       (
         product_locale_id, 
         package_detail_locale_id, 
-        quantity_price_model, 
-        user_id,
-        discount, 
+        user_id, 
+        quantity_price_model,
+        discount,
         total
-      ) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;`,
-      [
-        req.body.product_locale_id,
-        req.body.package_detail_locale_id,
-        req.body.user_id,
-        JSON.stringify(req.body.quantity_price_model),
-        req.body.discount,
-        req.body.total
-      ]
+      ) 
+      VALUES %L returning *`,
+      purchaseValues
     );
 
-    res.send(rows[0]);
+    const purchaseResponse = await pool.query(purchaseQuery);
+
+    await pool.query('COMMIT');
+
+    res.send(purchaseResponse.rows);
   } catch (err) {
     console.log(err);
+    await pool.query('ROLLBACK');
     res.status(400).json({
       err: err.message
     });
