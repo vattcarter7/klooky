d3dbd447136120210cca9ed14c0ffaa27a063fbd
vattcarter7 <vattcarter@gmail.com>
2021-05-04 11:23:21 +0700

    user table

--- server/migrations/1611485841264_user.js
+++ server/migrations/1611485841264_user.js
@@ -6,12 +6,16 @@ exports.up = (pgm) => {
   pgm.sql(`
     CREATE TABLE users (
       id                        SERIAL PRIMARY KEY,
-      email                     TEXT UNIQUE NOT NULL check (email ~* '^.+@.+\..+$'),
-      password                  VARCHAR(128) NOT NULL,
+      login_email               VARCHAR(150) UNIQUE check (login_email ~* '^.+@.+\..+$'),
+      login_password            VARCHAR(128),
+      signin_method             VARCHAR(20),
+      social_network_id         BIGINT,
       firstname                 VARCHAR(100) NOT NULL,
       lastname                  VARCHAR(100) NOT NULL,
       gender                    VARCHAR(10) NOT NULL,
       user_role                 VARCHAR(50) DEFAULT 'user',
+      contact_email             VARCHAR(150) check (login_email ~* '^.+@.+\..+$'),
+      contact_phone             VARCHAR(30),
       active                    BOOLEAN DEFAULT true,
       password_reset_token      VARCHAR(200),
       password_reset_expires    TIMESTAMPTZ,
