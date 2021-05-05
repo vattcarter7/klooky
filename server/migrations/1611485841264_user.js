960bf6d707fba838da60f82927891ff4d8273b56
vattcarter7 <vattcarter@gmail.com>
2021-05-05 14:04:30 +0700

    user table migration

--- server/migrations/1611485841264_user.js
+++ server/migrations/1611485841264_user.js
@@ -4,17 +4,19 @@ exports.shorthands = undefined;
 
 exports.up = (pgm) => {
   pgm.sql(`
+    CREATE TYPE sigin_in_method_type AS ENUM ('email', 'facebook', 'google');
+
     CREATE TABLE users (
       id                        SERIAL PRIMARY KEY,
       login_email               VARCHAR(150) UNIQUE check (login_email ~* '^.+@.+\..+$'),
       login_password            VARCHAR(128),
-      signin_method             VARCHAR(20),
-      social_network_id         BIGINT,
-      firstname                 VARCHAR(100) NOT NULL,
-      lastname                  VARCHAR(100) NOT NULL,
-      gender                    VARCHAR(10) NOT NULL,
-      user_role                 VARCHAR(50) DEFAULT 'user',
-      contact_email             VARCHAR(150) check (login_email ~* '^.+@.+\..+$'),
+      signin_method             sigin_in_method_type,
+      social_network_user_id    BIGINT,
+      firstname                 VARCHAR(100),
+      lastname                  VARCHAR(100),
+      gender                    VARCHAR(10),
+      user_role                 VARCHAR(50) DEFAULT 'subscriber',
+      contact_email             VARCHAR(150) check (contact_email ~* '^.+@.+\..+$'),
       contact_phone             VARCHAR(30),
       active                    BOOLEAN DEFAULT true,
       password_reset_token      VARCHAR(200),
@@ -23,9 +25,10 @@ exports.up = (pgm) => {
       updated_at                TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
     );
 
-    INSERT INTO users (email, password, firstname, lastname, gender) 
-    VALUES ('sopheak@gmail.com', '123456', 'vatt', 'sopheak', 'male'),
-           ('katie@gmail.com', '123456', 'katie', 'pocha', 'female');
+    INSERT INTO users (login_email, login_password, firstname, lastname, gender, signin_method) 
+    VALUES ('sopheak@gmail.com', '123456', 'vatt', 'sopheak', 'male', 'email'),
+           ('katie@gmail.com', '123456', 'katie', 'pocha', 'female', 'email'),
+           ('david@gmail.com', '123456', 'david', 'miles', 'female', 'facebook');
 
   `);
 };
