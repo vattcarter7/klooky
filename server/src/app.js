d73067db7fdee47c405204aa3271c255a792a3af
vattcarter7 <vattcarter@gmail.com>
2021-05-05 15:04:55 +0700

    implemented social strategy

--- server/src/app.js
+++ server/src/app.js
@@ -1,15 +1,57 @@
 const express = require('express');
 const morgan = require('morgan');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 const { readdirSync } = require('fs');
+const passport = require('passport');
+const FacebookStrategy = require('passport-facebook').Strategy;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 module.exports = () => {
+  passport.serializeUser((user, cb) => {
+    cb(null, user);
+  });
+
+  passport.deserializeUser((user, cb) => {
+    cb(null, user);
+  });
+
+  // Facebook Strategy
+  passport.use(
+    new FacebookStrategy(
+      {
+        clientID: keys.FACEBOOK.clientID,
+        clientSecret: keys.FACEBOOK.clientSecret,
+        callbackURL: '/auth/facebook/callback'
+      },
+      (accessToken, refreshToken, profile, cb) => {
+        console.log(chalk.blue(JSON.stringify(profile)));
+        user = { ...profile };
+        return cb(null, profile);
+      }
+    )
+  );
+
+  // Google Strategy
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: keys.GOOGLE.clientID,
+        clientSecret: keys.GOOGLE.clientSecret,
+        callbackURL: '/auth/google/callback'
+      },
+      (accessToken, refreshToken, profile, cb) => {
+        console.log(chalk.blue(JSON.stringify(profile)));
+        user = { ...profile };
+        return cb(null, profile);
+      }
+    )
+  );
+
   const app = express();
 
   // middlewares
   app.use(morgan('dev'));
-  app.use(bodyParser.json({ limit: '2mb' }));
+  app.use(express.json());
   app.use(cors());
 
   // routes middleware
