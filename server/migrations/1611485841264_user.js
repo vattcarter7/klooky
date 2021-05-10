0dc8643aca69bfcd4ce92ce7c621ba62b441b0fa
vattcarter7 <vattcarter@gmail.com>
2021-05-11 02:23:06 +0700

    user role enum

--- server/migrations/1611485841264_user.js
+++ server/migrations/1611485841264_user.js
@@ -5,17 +5,19 @@ exports.shorthands = undefined;
 exports.up = (pgm) => {
   pgm.sql(`
     CREATE TYPE sigin_in_method_type AS ENUM ('email', 'facebook', 'google');
-    CREATE TYPE gender_type AS ENUM ('male', 'female', 'other');
+    CREATE TYPE gender_type AS ENUM ('male', 'female');
+    CREATE TYPE user_role_type AS ENUM ('admin', 'subscriber', 'guide', 'establisher');
 
     CREATE TABLE users (
       id                        SERIAL PRIMARY KEY,
+      avatar                    TEXT,
       login_email               VARCHAR(150) UNIQUE check (login_email ~* '^.+@.+\..+$'),
       login_password            VARCHAR(128),
       signin_method             sigin_in_method_type,
       social_network_user_id    VARCHAR(100),
-      fullname                  VARCHAR(100),
+      fullname                  VARCHAR(55),
       gender                    gender_type,
-      user_role                 VARCHAR(50) DEFAULT 'subscriber',
+      user_role                 user_role_type DEFAULT 'subscriber',
       contact_email             VARCHAR(150) check (contact_email ~* '^.+@.+\..+$'),
       contact_phone             VARCHAR(30),
       active                    BOOLEAN DEFAULT true,
@@ -25,10 +27,13 @@ exports.up = (pgm) => {
       updated_at                TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
     );
 
-    INSERT INTO users (login_email, login_password, fullname, gender, signin_method) 
-    VALUES ('sopheak@gmail.com', '123456', 'vatt sopheak', 'male', 'email'),
-           ('katie@gmail.com', '123456', 'katie pocha', 'female', 'email'),
-           ('david@gmail.com', '123456', 'david miles', 'female', 'facebook');
+    ALTER TABLE users ADD CONSTRAINT signin_method_social_network_user_id UNIQUE (signin_method, social_network_user_id);
+
+    INSERT INTO users (login_email, login_password, fullname, gender, signin_method, user_role) 
+    VALUES ('sopheak@gmail.com', '123456', 'vatt sopheak', 'male', 'email', 'admin'),
+           ('katie@gmail.com', '123456', 'katie pocha', 'female', 'email', 'guide'),
+           ('david@gmail.com', '123456', 'david miles', 'female', 'facebook', 'establisher'),
+           ('kon@gmail.com', '123456', 'kon york', 'male', 'facebook', 'subscriber');
 
   `);
 };
