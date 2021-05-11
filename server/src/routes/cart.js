77ff187f2c3138396c1f4e9458ea6cf253c64885
vattcarter7 <vattcarter@gmail.com>
2021-05-12 02:55:52 +0700

    protect middleware

--- server/src/routes/cart.js
+++ server/src/routes/cart.js
@@ -5,6 +5,7 @@ const { validate } = require('../validators');
 const { createCartCheckRules } = require('../validators/cart/cart-check-rules');
 
 // middlewares
+const { protect } = require('../middlewares/auth');
 
 // controller
 const { createCartItem } = require('../controllers/cart');
@@ -12,6 +13,6 @@ const { createCartItem } = require('../controllers/cart');
 // @desc      make a cart
 // @route     POST /api/cart
 // @access    Private
-router.post('/cart', [createCartCheckRules, validate], createCartItem);
+router.post('/cart', protect, [createCartCheckRules, validate], createCartItem);
 
 module.exports = router;
