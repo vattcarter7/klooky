a5cd21ade6b8b6bfb6bb1186f2ae8e4b8eca91c5
vattcarter7 <vattcarter@gmail.com>
2021-05-12 02:55:13 +0700

    auth middleware

--- server/src/middlewares/auth.js
+++ server/src/middlewares/auth.js
@@ -5,6 +5,7 @@ const pool = require('../pool');
 // Protect routes
 exports.protect = async (req, res, next) => {
   let token;
+  // console.log(req.cookies.jwt);
 
   if (
     req.headers.authorization &&
@@ -12,9 +13,6 @@ exports.protect = async (req, res, next) => {
   ) {
     // Set token from Bearer token in header
     token = req.headers.authorization.split(' ')[1];
-  } else if (req.cookies.jwt && req.cookies.jwt !== 'loggedout') {
-    // Set token from cookie
-    token = req.cookies.jwt;
   }
 
   if (!token) {
@@ -23,44 +21,45 @@ exports.protect = async (req, res, next) => {
       .json({ errorMsg: 'Not authorized to access this route' });
   }
 
-  try {
-    // verify token
-    const decoded = await promisify(jwt.verify)(
-      token,
-      process.env.JWT_PRIVATE_KEY
-    );
-    const queryText = 'SELECT * FROM users WHERE id = $1';
-    const { rows } = await pool.query(queryText, [decoded.userId]);
-    if (!rows[0]) {
-      return res
-        .status(403)
-        .json({ errorMsg: 'Not authorized to access this route' });
-    }
+  console.log('token:', token);
 
-    // GRAND ACCESS TO PROTECTED ROUTE
-    req.user = {
-      id: decoded.userId,
-      fullname: rows[0].fullname,
-      user_role: rows[0].user_role,
-      signin_method: rows[0].signin_method
-    };
-    next();
-  } catch (err) {
-    return res
-      .status(403)
-      .json({ errorMsg: 'Not authorized to access this route' });
-  }
+  jwt.verify(token, process.env.JWT_PRIVATE_KEY, async (error, decoded) => {
+    if (error) {
+      return res.status(403).json({
+        msg: 'Token is not valid. Not authorized to access this route'
+      });
+    } else {
+      try {
+        // check if the user is still in use
+        const { rows } = await pool.query(
+          'select * from users where id = $1 and banned = $2',
+          [decoded.userId, false]
+        );
+        if (!rows[0]) {
+          return res.status(403).json({
+            msg: 'Token is not valid. Not authorized to access this route ^^^'
+          });
+        }
+        // GRANT ACCESS TO PROTECTED ROUTE
+        req.user = decoded;
+        console.log('req.user:', req.user);
+        next();
+      } catch (err) {
+        return res.status(403).json({
+          msg: 'Token is not valid. Not authorized to access this route !!!'
+        });
+      }
+    }
+  });
 };
 
 // Grand access to specific roles
 exports.authorize = (...roles) => {
   return (req, res, next) => {
     if (!roles.includes(req.user.user_role)) {
-      return res
-        .status(403)
-        .json({
-          errorMsg: `User role ${req.user.user_role} is not authorized to this route`
-        });
+      return res.status(403).json({
+        errorMsg: `User role ${req.user.user_role} is not authorized to this route`
+      });
     }
     next();
   };
