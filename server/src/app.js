1da8f6d7123c1b723ad5dcb00f4d2dd6a1991d8d
vattcarter7 <vattcarter@gmail.com>
2021-05-14 23:13:45 +0700

    added security on app.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const path = require('path');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const compression = require('compression');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const { readdirSync } = require('fs');
@@ -9,10 +15,55 @@ const errorHandler = require('./middlewares/error');
 const app = express();
 
 module.exports = () => {
-  app.use(morgan('dev'));
-  app.use(express.json());
+  // Development logging
+  if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+  }
+  // Body parser, reading data from body into req.body
+  app.use(express.json({ limit: '10kb' }));
+  app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+
   app.use(cookieParser());
+
   app.use(cors());
+
+  app.enable('trust proxy');
+
+  app.set('view engine', 'pug');
+  app.set('views', path.join(__dirname, 'views'));
+
+  // Serving static files
+  app.use(express.static(path.join(__dirname, 'public')));
+
+  // Set security HTTP headers
+  app.use(helmet());
+
+  // Limit requests from same API
+  const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many requests from this IP, please try again in an hour!'
+  });
+  app.use('/api', limiter);
+
+  // Data sanitization against XSS
+  app.use(xss());
+
+  // Prevent parameter pollution
+  app.use(
+    hpp({
+      whitelist: [
+        'duration',
+        'ratingsQuantity',
+        'ratingsAverage',
+        'maxGroupSize',
+        'difficulty',
+        'price'
+      ]
+    })
+  );
+
+  app.use(compression());
 
   // routes middleware
   readdirSync('./src/routes').map((r) =>
