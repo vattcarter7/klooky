bcef76089b1824cf4133c8565fc1a778d58c3c78
vattcarter7 <vattcarter@gmail.com>
2021-05-16 21:06:50 +0700

    package_price_locale

--- server/migrations/1616687716968_package-price-locale.js
+++ server/migrations/1616687716968_package-price-locale.js
@@ -6,13 +6,13 @@ exports.up = (pgm) => {
   pgm.sql(`
     CREATE TABLE package_price_locale (
       id SERIAL PRIMARY KEY,
+      package_price_name VARCHAR NOT NULL,
       package_detail_locale_id INT REFERENCES package_detail_locale(id) NOT NULL,
       min_age INT NOT NULL,
       max_age INT NOT NULL,
       min_pax INT NOT NULL,
       max_pax INT NOT NULL,
       is_with_adult_required BOOLEAN DEFAULT FALSE,
-      package_price_name VARCHAR NOT NULL,
       price REAL NOT NULL,
       published BOOLEAN DEFAULT FALSE,
       created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
