fd64921f64f5dfcce01289e1c66be63d3d3d9583
vattcarter7 <vattcarter@gmail.com>
2021-05-16 19:49:18 +0700

    fixed bug

--- server/migrations/1616687716968_package-price-locale.js
+++ server/migrations/1616687716968_package-price-locale.js
@@ -6,7 +6,7 @@ exports.up = (pgm) => {
   pgm.sql(`
     CREATE TABLE package_price_locale (
       id SERIAL PRIMARY KEY,
-      package_id INT REFERENCES package(id) NOT NULL,
+      package_detail_locale_id INT REFERENCES package_detail_locale(id) NOT NULL,
       language_id INT REFERENCES language(id) NOT NULL,
       min_age INT NOT NULL,
       max_age INT NOT NULL,
@@ -19,15 +19,15 @@ exports.up = (pgm) => {
       updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
     );
 
-    CREATE INDEX package_price_locale_package_id_idx ON package_price_locale(package_id);
+    CREATE INDEX package_price_locale_package_detail_locale_id_idx ON package_price_locale(package_detail_locale_id);
 
     CREATE INDEX package_price_locale_language_id_idx ON package_price_locale(language_id);
 
-    INSERT INTO package_price_locale (package_id, language_id, package_price_name, price, min_age, max_age, min_pax, max_pax, is_with_adult_required)
+    INSERT INTO package_price_locale (package_detail_locale_id, language_id, package_price_name, price, min_age, max_age, min_pax, max_pax, is_with_adult_required)
     VALUES (1, 1, 'adult', 15.75, 18, 100, 5, 10, false),
-           (1, 1, 'kid', 7, 3, 11, 5, 10, true),
+           (2, 1, 'kid', 7, 3, 11, 5, 10, true),
            (1, 2, '성인', 15.75, 18, 100, 5, 10, false),
-           (1, 2, '아동', 7, 3, 11, 5, 10, true);
+           (2, 2, '아동', 7, 3, 11, 5, 10, true);
           
   `);
 };
