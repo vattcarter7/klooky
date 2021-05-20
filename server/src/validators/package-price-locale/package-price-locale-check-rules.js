f575bdd313ae6b675f3b73a072067286e29d3ad2
vattcarter7 <vattcarter@gmail.com>
2021-05-20 16:13:36 +0700

    validate package price locale

--- server/src/validators/package-price-locale/package-price-locale-check-rules.js
+++ server/src/validators/package-price-locale/package-price-locale-check-rules.js
@@ -2,8 +2,10 @@ const { check } = require('express-validator');
 
 exports.createPackagePriceLocaleCheckRules = (() => {
   return [
-    check('package_id').not().isEmpty().withMessage('package ID is required'),
-    check('language_id').not().isEmpty().withMessage('language ID is required'),
+    check('package_detail_locale_id')
+      .not()
+      .isEmpty()
+      .withMessage('package detail local ID is required'),
     check('package_price_name')
       .isString()
       .withMessage('a valid package price name is required'),
