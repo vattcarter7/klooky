6cc1d148de2ee666e16f397908675e4bc6748967
vattcarter7 <vattcarter@gmail.com>
2021-05-21 14:42:42 +0700

    API URL constants

---
+++ client-web/src/constants/index.js
@@ -0,0 +1,6 @@
+// API URL
+export const CATEGORY_URL = '/api/category';
+export const AUTH_URL = '/api/auth';
+export const PRODUCT_URL = '/api/product';
+export const CART_URL = '/api/cart';
+export const PURCHASE_URL = '/api/purchase';
