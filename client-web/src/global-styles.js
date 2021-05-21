bd0b8369195188ec41814e278340584445f833c7
vattcarter7 <vattcarter@gmail.com>
2021-05-21 17:07:57 +0700

    a tag global style

--- client-web/src/global-styles.js
+++ client-web/src/global-styles.js
@@ -4,12 +4,19 @@ export const GlobalStyles = createGlobalStyle`
   *, *:before, *:after {
     box-sizing: border-box;
   }
+  
   html, body {
     height: 100%;
     font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     font-size: 16px;
-}`;
+  }
+
+  a {
+		text-decoration: none;
+		color: black;
+	}
+`;
 
 //TODO: add  background-color: #000000;  color: #333333; in the body of GlobalStyles
