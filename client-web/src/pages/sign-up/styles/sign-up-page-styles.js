8a3eb0506a86c6a072f4879be744796853887c17
vattcarter7 <vattcarter@gmail.com>
2021-05-23 03:26:59 +0700

    sign up page styles

--- client-web/src/pages/sign-up/styles/sign-up-page-styles.js
+++ client-web/src/pages/sign-up/styles/sign-up-page-styles.js
@@ -1,5 +1,33 @@
 import styled from 'styled-components/macro';
+import { Link } from 'react-router-dom';
+
+import { MEDIUM_BLUE, DARK_BLUE } from '../../../constants/colors';
 
 export const SignUpContainer = styled.div`
   display: flex;
+  flex-direction: column;
+  width: 380px;
+  margin-right: auto;
+  margin-left: auto;
+`;
+
+export const SignUpTitle = styled.h2`
+  margin: 10px 0;
+`;
+
+export const SignInLinkContainer = styled.div`
+  padding: 20px 0;
+  display: flex;
+`;
+
+export const SignInLink = styled(Link)`
+  margin-left: 5px;
+  margin-right: 5px;
+  cursor: pointer;
+  color: ${MEDIUM_BLUE};
+  text-decoration: underline;
+
+  &:hover {
+    color: ${DARK_BLUE};
+  }
 `;
