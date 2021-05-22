69d8ef0b8e1a1fa5549867bb635c74fc3062299c
vattcarter7 <vattcarter@gmail.com>
2021-05-23 01:25:24 +0700

    SignUpPage

---
+++ client-web/src/pages/sign-up/sign-up-page.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { SignUpContainer } from './styles/sign-up-page-styles';
+
+const SignUpPage = () => {
+  return <SignUpContainer>Sign Up Page</SignUpContainer>;
+};
+
+export default SignUpPage;
