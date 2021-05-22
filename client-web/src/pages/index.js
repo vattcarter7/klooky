dd5bf3d5df7a2dd5a27042725fa3b1ee31b77c0b
vattcarter7 <vattcarter@gmail.com>
2021-05-23 01:25:14 +0700

    export default SignUpPage on index

--- client-web/src/pages/index.js
+++ client-web/src/pages/index.js
@@ -1,2 +1,3 @@
 export { default as HomePage } from './home/home-page';
 export { default as SignInPage } from './sign-in/sign-in-page';
+export { default as SignUpPage } from './sign-up/sign-up-page';
