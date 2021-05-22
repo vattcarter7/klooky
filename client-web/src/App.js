9ed5aacdb49e84179033c7be93588b5220ee9140
vattcarter7 <vattcarter@gmail.com>
2021-05-23 01:08:20 +0700

    app

--- client-web/src/App.js
+++ client-web/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import HomePage from './pages/home/home-page';
+import SignInPage from './pages/sign-in/sign-in-page';
+
 import { Header } from './components';
 
 function App() {
   return (
-    <div className='App'>
+    <div>
       <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/signin' component={SignInPage} />
+      </Switch>
     </div>
   );
 }
--- client-web/src/components/index.js
+++ client-web/src/components/index.js
@@ -1,1 +1,5 @@
-export { default as Header } from './header/Header';
+export { default as Header } from './header/header';
+export { default as FormInput } from './form-input/form-input';
+export { default as SignIn } from './sign-in/sign-in';
+export { default as SignUp } from './sign-up/sign-up';
+export { default as CustomButton } from './custom-button/custom-button';
