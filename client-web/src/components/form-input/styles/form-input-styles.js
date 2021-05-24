ff69932f7ebddf56debb5963ab729601f85b3a46
vattcarter7 <vattcarter@gmail.com>
2021-05-24 23:24:46 +0700

    change colors

--- client-web/src/components/form-input/styles/form-input-styles.js
+++ client-web/src/components/form-input/styles/form-input-styles.js
@@ -1,17 +1,16 @@
 import styled, { css } from 'styled-components/macro';
 
-const subColor = 'grey';
-const mainColor = 'black';
+import { GREY_LIGHT, GREY_MEDIUM, BLACK_DARK } from '../../../constants/colors';
 
 const shrinkLabelStyles = css`
   top: -14px;
   font-size: 12px;
-  color: ${mainColor};
+  color: ${BLACK_DARK};
 `;
 
 export const GroupContainer = styled.div`
   position: relative;
-  margin: 45px 0;
+  margin: 25px 0;
   input[type='password'] {
     letter-spacing: 0.2em;
   }
@@ -20,27 +19,30 @@ export const GroupContainer = styled.div`
 export const FormInputContainer = styled.input`
   background: none;
   background-color: white;
-  color: ${subColor};
+  color: ${GREY_MEDIUM};
   font-size: 18px;
   padding: 10px 10px 10px 5px;
   display: block;
   width: 100%;
   border: none;
   border-radius: 0;
-  border-bottom: 1px solid ${subColor};
-  margin: 25px 0;
+  border-bottom: 1px solid ${GREY_MEDIUM};
+  margin: 0;
   &:focus {
     outline: none;
   }
   &:focus ~ label {
     ${shrinkLabelStyles}
   }
+  ${(props) =>
+    props.backgroundGrey &&
+    css`
+      background: ${GREY_LIGHT};
+    `}
 `;
 
-FormInputContainer.displayName = 'FormInputContainer';
-
 export const FormInputLabel = styled.label`
-  color: ${subColor};
+  color: ${GREY_MEDIUM};
   font-size: 16px;
   font-weight: normal;
   position: absolute;
@@ -52,5 +54,3 @@ export const FormInputLabel = styled.label`
     ${shrinkLabelStyles}
   }
 `;
-
-FormInputLabel.displayName = 'FormInputLabel';
