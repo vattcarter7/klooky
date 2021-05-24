a37baffa14d7e76072d1fed825d5c87c5b35e102
vattcarter7 <vattcarter@gmail.com>
2021-05-24 23:25:30 +0700

    removed signin and signup export default

--- client-web/src/components/index.js
+++ client-web/src/components/index.js
@@ -1,5 +1,3 @@
 export { default as Header } from './header/header';
 export { default as FormInput } from './form-input/form-input';
-export { default as SignIn } from './sign-in/sign-in';
-export { default as SignUp } from './sign-up/sign-up';
 export { default as CustomButton } from './custom-button/custom-button';
