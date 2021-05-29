7e65234e4bdb7f3f98bdebc6ec603d1a234e829d
vattcarter7 <vattcarter@gmail.com>
2021-05-29 18:16:44 +0700

    added profile link on header

--- client-web/src/components/header/Header.js
+++ client-web/src/components/header/Header.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+import { logOut } from '../../redux/user/user-action';
 
 import {
   HeaderContainer,
@@ -9,17 +11,36 @@ import {
   OptionLink
 } from './styles/header-styles';
 
-export const Header = () => (
-  <HeaderContainer>
-    <LogoContainer to='/'>
-      <Logo />
-    </LogoContainer>
-    <OptionsContainer>
-      <OptionLink to='/shop'>SHOP</OptionLink>
-      <OptionLink to='/contact'>CONTACT</OptionLink>
-      <OptionLink to='/signin'>SIGN IN</OptionLink>
-    </OptionsContainer>
-  </HeaderContainer>
-);
+const Header = () => {
+  const dispatch = useDispatch();
+  const { loading, user, isAuthenticated } = useSelector((state) => state.user);
+
+  const handleLogout = async (e) => {
+    e.preventDefault();
+    dispatch(logOut());
+  };
+
+  return (
+    <HeaderContainer>
+      <LogoContainer to='/'>
+        <Logo />
+      </LogoContainer>
+      <OptionsContainer>
+        <OptionLink to='/profile'>
+          {!loading && user && user.fullname}
+        </OptionLink>
+        <OptionLink to='/shop'>SHOP</OptionLink>
+        <OptionLink to='/contact'>CONTACT</OptionLink>
+        {!loading && !isAuthenticated ? (
+          <OptionLink to='/signin'>SIGN IN</OptionLink>
+        ) : (
+          <OptionLink to='/signout' onClick={handleLogout}>
+            SIGN OUT
+          </OptionLink>
+        )}
+      </OptionsContainer>
+    </HeaderContainer>
+  );
+};
 
 export default Header;
