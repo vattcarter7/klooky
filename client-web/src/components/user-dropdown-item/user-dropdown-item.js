b8d96610665251946b2bb5d7f8d1ec501df3e9b2
vattcarter7 <vattcarter@gmail.com>
2021-06-05 22:13:33 +0700

    hide user dropdown and push to other routes

--- client-web/src/components/user-dropdown-item/user-dropdown-item.js
+++ client-web/src/components/user-dropdown-item/user-dropdown-item.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
 import { UserDropdownItemContainer, Icon } from './styles/user-dropdown-styles';
 import { DROPDOWN_ITEMS } from '../../constants/user-dropdown';
+import { hideUserDropdown } from '../../redux/user/user-action';
 
 const UserDropdownItem = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const handleClick = (url) => {
+    dispatch(hideUserDropdown());
+    return history.push(`/${url}`);
+  };
+
   return DROPDOWN_ITEMS.map((item) => (
-    <UserDropdownItemContainer key={item.text}>
+    <UserDropdownItemContainer
+      key={item.text}
+      onClick={() => handleClick(item.link)}
+    >
       <Icon className={item.icon}></Icon> {item.text}
     </UserDropdownItemContainer>
   ));
