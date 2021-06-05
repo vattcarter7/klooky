a5479cc064c07d06bba0c077514c04043615db66
vattcarter7 <vattcarter@gmail.com>
2021-06-05 19:26:20 +0700

    Header by removing contact and profile

--- client-web/src/components/header/Header.js
+++ client-web/src/components/header/Header.js
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import emptyUser from '../../assets/user.png';
-import { logOut, showUserDropdown } from '../../redux/user/user-action';
+import { showUserDropdown } from '../../redux/user/user-action';
 
 import { UserDropdown } from '../../components';
 
@@ -21,11 +21,6 @@ const Header = () => {
     (state) => state.user
   );
 
-  const handleLogout = async (e) => {
-    e.preventDefault();
-    dispatch(logOut());
-  };
-
   const handleShowUserDropdown = () => {
     setTimeout(() => {
       dispatch(showUserDropdown());
@@ -38,18 +33,11 @@ const Header = () => {
         <Logo />
       </LogoContainer>
       <OptionsContainer>
-        <OptionLink to='/profile'>
-          {!loading && user && user.fullname}
-        </OptionLink>
         <OptionLink to='/shop'>SHOP</OptionLink>
-        <OptionLink to='/contact'>CONTACT</OptionLink>
         {!loading && !isAuthenticated ? (
           <OptionLink to='/signin'>SIGN IN</OptionLink>
         ) : (
           <>
-            <OptionLink to='#' onClick={handleLogout}>
-              SIGN OUT
-            </OptionLink>
             <OptionLink to='#' onClick={handleShowUserDropdown}>
               {user && user.avatar ? (
                 <Avatar src={user.avatar} />
