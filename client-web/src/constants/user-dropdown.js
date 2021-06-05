d200b19fbe11f7579de779df4b2ce4d7757fc8e1
vattcarter7 <vattcarter@gmail.com>
2021-06-05 14:50:28 +0700

    user dropdown data

---
+++ client-web/src/constants/user-dropdown.js
@@ -0,0 +1,8 @@
+export const DROPDOWN_ITEMS = [
+  { icon: 'fa fa-bookmark-o', text: 'Booking', link: '#' },
+  { icon: 'fa fa-gift', text: 'Gift card', link: '#' },
+  { icon: 'fa fa-heart-o', text: 'Wishlist', link: '#' },
+  { icon: 'fa fa-pencil-square-o', text: 'Reviews', link: '#' },
+  { icon: 'fa fa-magic', text: 'Rewards', link: '#' },
+  { icon: 'fa fa-sliders', text: 'Setting', link: '#' }
+];
