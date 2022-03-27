f44d1edc383053716c8eb2846008bd3b2f51cef2
vatt sopheak <vattcarter@gmail.com>
2022-03-27 12:07:39 +0700

    Added sample.env

--- server/migrations/1611485841266_product.js
+++ server/migrations/1611485841266_product.js
@@ -8,7 +8,7 @@ exports.up = (pgm) => {
       id SERIAL PRIMARY KEY,
       product_duration INT NOT NULL, 
       product_location JSONB,
-      product_free_cancelation_max_day INT NOT NULL DEFAULT 0,
+      product_free_cancellation_max_day INT NOT NULL DEFAULT 0,
       product_validity_period INT NOT NULL DEFAULT 0,
       is_pickup BOOLEAN NOT NULL DEFAULT FALSE,
       is_fixed_date_ticket BOOLEAN NOT NULL DEFAULT FALSE,
@@ -21,7 +21,7 @@ exports.up = (pgm) => {
       updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
     );
 
-    INSERT INTO product (product_duration, product_location, product_free_cancelation_max_day)
+    INSERT INTO product (product_duration, product_location, product_free_cancellation_max_day)
     VALUES (2, '[{ "lat": 123456789, "lng": 987654321 }]', 7),
            (8, '[{ "lat": 123456789, "lng": 987654321 }]', 5);
   `);
